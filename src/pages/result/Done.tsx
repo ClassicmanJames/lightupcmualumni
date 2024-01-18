0a969b1549f3b649be2076a3187c6aa384ee078e
Classicman <jamesr.com@gmail.com>
2024-01-18 16:00:36 +0700

    v1

--- src/pages/result/Done.tsx
+++ src/pages/result/Done.tsx
@@ -3,14 +3,23 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import * as Setting from "@/redux/reducers/setting";
 import { useSelector, useDispatch } from "react-redux";
+import { useQRCode } from "next-qrcode";
+import { useParams, useSearchParams } from "next/navigation";
 
 const Done = () => {
+  const { SVG } = useQRCode();
   const dispath = useDispatch();
   dispath(Setting.updateState({ isLoadingScreen: false }));
   const [yearuser, setYearuser] = useState<string>();
-
+  const searchParams = useSearchParams();
   dispath(Setting.updateState({ isLoadingScreen: false }));
-
+  const [tkData, setTkData] = useState<string>();
+  const token = searchParams.get("token");
+  useEffect(() => {
+    if (token) {
+      setTkData(token);
+    }
+  }, [token]);
   return (
     <div className="m-5 h-50 bg-[#FBFBFB] drop-shadow-xl rounded-xl p-2">
       <div className="grid m-5  rounded-lg justify-items-center">
@@ -22,9 +31,22 @@ const Done = () => {
         ></Image>
       </div>
       <div className="h-50  rounded-lg  text-green-500  text-center">
-        <div className="bg-green-100 p-2 rounded-lg">
-          ลงทะเบียนเสร็จสิ้นกรุณารอการยืนยืนจากระบบ <br /> (Your registration is
-          complete. Please wait for the confirmation from the system.)
+        <div className="p-2 bg-green-100 rounded-lg">
+          โปรดแสดง Qr Code นี้ต่อเจ้าหน้าก่อนเข้างาน <br /> (Please show this Qr
+          Code to the staff before entering the event.)
+        </div>
+        <div className="flex justify-center">
+          <SVG
+            text={tkData || "3"}
+            options={{
+              margin: 2,
+              width: 200,
+              color: {
+                dark: "#000000",
+                light: "#FFFFFF",
+              },
+            }}
+          />
         </div>
       </div>
     </div>
